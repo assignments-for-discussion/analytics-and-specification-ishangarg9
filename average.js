8100dad9167e39343c5d58a66f90ee62de4e5d39
ishangarg9 <67002620+ishangarg9@users.noreply.github.com>
2021-12-09 15:16:45 +0530

    Update average.js
    code correction test 1

--- average.js
+++ average.js
@@ -1,6 +1,8 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  const sum =  numbers.reduce((p, c)=> p + c, 0);
+  const avg = (sum / numbers.length) || 0;
+  return avg; 
 }
 
 module.exports = {average};
