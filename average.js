2a0b2beb8dc1b0974dffdeab5d7ecdd209de847d
ishangarg9 <67002620+ishangarg9@users.noreply.github.com>
2021-12-10 01:34:06 +0530

    Update average.js
    test7

--- average.js
+++ average.js
@@ -4,14 +4,18 @@ function average(numbers) {
   var n = numbers.length;
   if(n == 0) return NaN; // for empty array
 
+  var sum = 0;
   for(let i=0; i < n; i++){
     if(isNaN(numbers[i])) {
       numbers.splice(i, 1);
       n = n - 1;
     }
+    else{
+      sum = sum + numbers[i];
+    }
   }
 
-  const avg = numbers.reduce((p, c)=> p + c, 0) / n;
+  const avg = sum / n;
 
   //approximating average
   const apx = parseFloat(avg.toFixed(2));
